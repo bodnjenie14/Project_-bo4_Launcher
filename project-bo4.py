c409ceb79d45551100a060a601faa29585d321a8
bodnjenie14 <126781031+bodnjenie14@users.noreply.github.com>
2023-10-27 16:38:45 +0100

    Update project-bo4.py

--- project-bo4.py
+++ project-bo4.py
@@ -12,21 +12,50 @@ try:
 except Exception as e:
     print(f"Error while removing dll files: {e}")
 
-from PyQt5.QtWidgets import QApplication, QMessageBox, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QDialog, QStyle
-from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QMessageBox, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QDialog, QStyle, QCheckBox
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtGui import QPixmap, QIcon, QMovie, QPalette
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import Qt, QUrl, QTimer
 import subprocess
 import shutil
 import random
 import json
 import sys
 
-def get_json_item(json_path, spot, name):
-    with open(json_path, "r") as json_file:
-        data = json.load(json_file)
-        print(data[spot][name])
-    return data[spot][name]
+global reshade
+reshade = False
 
+def get_json_item(json_path, spot, name):
+    try:
+        if os.path.exists(json_path):
+            with open(json_path, "r") as json_file:
+                data = json.load(json_file)
+                print(data[spot][name])
+        else:
+            print("Creating default json")
+
+            data = {
+                "demonware":
+                {
+                    "ipv4": "127.0.0.1"
+                },
+                "identity":
+                {
+                    "name":  f'{os.getlogin()}',
+                }
+            }
+            json_data = json.dumps(data, indent=4) 
+
+            with open(json_path, "w") as json_file:
+                json_file.write(json_data)
+
+            return get_json_item(json_path, spot, name)
+
+        return data[spot][name]
+    except Exception as e:
+        print(f"Something went wrong while reading json file: {e}")
+        
 class change_name(QDialog):
     def __init__(self, parent=None):
         super(change_name, self).__init__(parent)
@@ -137,12 +166,15 @@ class change_ip(QDialog):
         """)
 
 def replace_json_value(json_file_path, value, spot, key):
-    with open(json_file_path, 'r') as json_file:
-        data = json.load(json_file)
-        data[spot][key] = value
+    try:
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+            data[spot][key] = value
 
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4) 
+        with open(json_file_path, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+    except Exception as e:
+        print(f"Something went wrong while modifying Json file: {e}")
 
 def set_ip():
     dialog = change_ip()
@@ -163,21 +195,57 @@ def set_ip():
         if ip_address != "":
             replace_json_value("project-bo4.json", ip_address, 'demonware', 'ipv4')
 
-    return result
+    return result    
 
 def start_game(which):
 
+    global reshade
+    
+    if not os.path.exists("Players\\Mp.cfg"):
+        if not os.path.exists("Players"):
+            os.mkdir("Players")
+        
+        try:
+            path_to_cfg = f"{os.getcwd()}\\files\\Players\\Mp.cfg"
+            shutil.copy(path_to_cfg, os.getcwd())
+        except Exception as e:
+            print(e)
+
+    if not os.path.exists("LPC\\.manifest") or not os.path.exists("LPC\\core_ffotd_tu23_639_cf92ecf4a75d3f79.ff") or not os.path.exists("LPC\\core_playlists_tu23_639_cf92ecf4a75d3f79.ff"):
+        if os.path.exists(f"{os.getcwd()}\\LPC"):
+            try:
+                shutil.rmtree(f"{os.getcwd()}\\LPC")
+            except Exception as e:
+                print(e)
+        
+        path_to_lpc = f"{os.getcwd()}\\files\\LPC"
+        try:
+            shutil.copytree(path_to_lpc, f"{os.getcwd()}\\LPC")
+        except Exception as E:
+            print(f"Error copying files: {E}")
+
     if which == "solo":
         try:
-            if os.path.exists(os.getcwd() + "/files/solo/d3d11.dll"):
-                shutil.copy(os.getcwd() + "/files/solo/d3d11.dll", os.getcwd())
+
+            if reshade == True:
+                path_to_dll = f"{os.getcwd()}\\files\\reshade_solo\\powrprof.dll"
+            else:
+                path_to_dll = f"{os.getcwd()}\\files\\solo\\d3d11.dll"
+
+            if os.path.exists(path_to_dll):
+                shutil.copy(path_to_dll, os.getcwd()) 
         except Exception as e:
             print(f"Error while copying dll files: {e}")
 
     elif which == "multi":
         try:
-            if os.path.exists(os.getcwd() + "/files/mp/d3d11.dll"):
-                shutil.copy(os.getcwd() + "/files/mp/d3d11.dll", os.getcwd())
+            if reshade == True:
+                path_to_dll = f"{os.getcwd()}\\files\\reshade_mp\\powrprof.dll"
+            else:
+                path_to_dll = f"{os.getcwd()}\\files\\mp\\d3d11.dll"
+
+            if os.path.exists(path_to_dll):
+                shutil.copy(path_to_dll, os.getcwd())
         except Exception as e:
             print(f"Error while copying dll files: {e}")
 
@@ -189,19 +257,37 @@ def start_game(which):
                 return
 
             if ip_address == "" and click == QDialog.Accepted:
-                start_game("Multiplayer")
+                start_game("multi")
                 return
             else:
                 return
 
     try:
         window.hide()
-        process = subprocess.Popen(["BlackOps4.exe"])
-        process.wait()
+
+        try:
+            window.player.stop()
+        except Exception as e:
+            print(f"Error: {e}")
+
+        try:
+            process = subprocess.Popen(["BlackOps4.exe"])
+            process.wait()
+        except Exception as e:
+            print(f"Error: {e}")
+
         sys.exit()
     except Exception as e:
         print(f"Error: {e}")
 
+def checkbox_state_change(state):
+    global reshade
+    if state == 2:
+        reshade = True
+        print("Checkbox is checked")
+    else:
+        print("Checkbox is unchecked")
+        reshade = False
 
 class launcher(QWidget):
     def __init__(self):
@@ -209,33 +295,76 @@ class launcher(QWidget):
         self.initUI()
         
     def initUI(self):
-        
+
         layout = QVBoxLayout()
-        try:
-
-            image_folder = os.path.join(os.getcwd(), "files/images")
-            image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(( '.jpg', '.jpeg', '.gif', '.bmp'))]
-            
-            random_image = random.choice(image_files)
-            
-            image_path = os.path.join(image_folder, random_image)
-
-            background_label = QLabel(self)
-            background_image = QPixmap(image_path)
-            background_label.setPixmap(background_image)
-            background_label.setGeometry(0, 0, background_image.width(), background_image.height())
-            self.setFixedSize(background_image.width(), background_image.height())
-        except Exception as e:
-            print(e)
-
         self.setWindowTitle('Project-BO4')
-        self.setWindowIcon(QIcon('files/images/exe_icon_bo4.ico'))
+        self.setWindowIcon(QIcon('files\\images\\exe_icon_bo4.ico'))
         title_layout = QVBoxLayout()
-        self.title = QLabel("")
-        title_layout.addWidget(self.title)
-        layout.addLayout(title_layout)
+        try:
+            
+            images_folder = os.path.join(os.getcwd(), "files\\images")
+            files = [f for f in os.listdir(images_folder) if f.lower().endswith(( '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png'))]
+            random_background = random.choice(files)
+        
+            if random_background.lower().endswith(('.gif')):
 
-        buttons_layout = QHBoxLayout()
+                self.movie = QMovie(os.path.join(os.getcwd(), f"files\\images\\{random_background}"))
+                background_label = QLabel(self)
+                layout.addWidget(background_label)
+                background_label.setMovie(self.movie)
+                self.movie.start()
+
+                self.setStyleSheet("background-color: #333;")
+
+                buttons_layout = QHBoxLayout()
+                checkbox = QCheckBox("Reshade")
+                buttons_layout.addWidget(checkbox)
+                checkbox.stateChanged.connect(checkbox_state_change)
+
+            elif random_background.lower().endswith(( '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png' )):
+
+                background_label = QLabel(self)
+                background_image = QPixmap(os.path.join(os.getcwd(), f"files\\images\\{random_background}"))
+                background_label.setPixmap(background_image)
+                background_label.setGeometry(0, 0, background_image.width(), background_image.height())
+                self.setFixedSize(background_image.width(), background_image.height())
+
+                self.title = QLabel("")
+                title_layout.addWidget(self.title)
+                layout.addLayout(title_layout)
+
+                check_box = QVBoxLayout()
+        
+                checkbox = QCheckBox("Reshade")
+                check_box.addWidget(checkbox)
+
+                checkbox.stateChanged.connect(checkbox_state_change)
+                layout.addLayout(check_box)
+            
+            #Add startup sound
+            try:
+                sound_folder = os.path.join(os.getcwd(), "files\\sounds")
+                sound_files = [f for f in os.listdir(sound_folder) if f.lower().endswith('.mp3')]
+                random_sound_file = random.choice(sound_files)
+
+                sound_path = os.path.join(sound_folder, random_sound_file)
+
+                self.player = QMediaPlayer()
+                sound_url = QUrl.fromLocalFile(sound_path)
+                content = QMediaContent(sound_url)
+                self.player.setMedia(content)
+                self.player.setVolume(30)
+                self.player.play()
+            
+            except Exception as e:
+                print(f"Something went wrong with start-up sound files: {e}")
+            
+        except Exception as e:
+            print(f"Something went wrong with start up image, gif or sound files: {e}")
+
+        
+        if not random_background.lower().endswith(('.gif')):
+            buttons_layout = QHBoxLayout()
 
         self.name = QPushButton("Change Name")
         self.name.clicked.connect(set_name)
@@ -263,6 +392,14 @@ class launcher(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    app.setStyle("Fusion")  # You can also use "Windows", "Macintosh", or "Fusion" for cross-platform style
+    palette = app.palette()
+    palette.setColor(QPalette.Window, Qt.black)  # Set the window background color
+    palette.setColor(QPalette.WindowText, Qt.white)  # Set the window text color
+    app.setPalette(palette)
+
+
     window = launcher()
     window.show()
     sys.exit(app.exec_())
