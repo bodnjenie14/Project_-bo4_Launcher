5b9b85154ac19a2008d9858b32c92d6ee99490d9
Pea <130810875+NotNierPea@users.noreply.github.com>
2025-09-07 17:21:39 +0300

    boo

--- source/launcher/launcher_funcs/auto_update.cpp
+++ source/launcher/launcher_funcs/auto_update.cpp
@@ -14,7 +14,7 @@
 
 namespace updater {
 
-    const std::string SERVER_VERSION = "1.0.20.6.1";
+    const std::string SERVER_VERSION = "1.0.20.7";
 
 	//test repo for testing update soon as i fluffed it
     //const std::string GITHUB_API_URL = "https://api.github.com/repos/bodnjenie14/bo4-test/releases/latest";
